00f05b09b7209b7956a071d170afc692207c1e15
Mateus Bougleux <mateusbougleuxmaia@hotmail.com>
2020-10-12 23:29:19 -0300

    Procedure code;

---
+++ sql/procedure.sql
@@ -0,0 +1,29 @@
+-- BEGINNING OF THE CREATION SECTION
+
+DROP PROCEDURE IF EXISTS insertProcedure;
+
+delimiter $$
+
+CREATE PROCEDURE insertProcedure()
+     BEGIN
+            -- VAR DECLARATION 
+            -- @i is the var to delimit the while
+            DECLARE i INT DEFAULT 0;
+
+            -- @randNumber é um valor aleatório
+            DECLARE randNumber INT DEFAULT 0;
+
+
+         WHILE (i<=999999) DO
+            SET randNumber=i+1;
+            -- HERE YOU PASTE THE SQL INSERT
+         END WHILE;
+
+ END$$
+
+-- END OF THE CRIATION SECTION
+
+-- EXECUTE PROCEDURE
+ delimiter ;
+
+call insertProcedure();
